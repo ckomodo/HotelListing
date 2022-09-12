a3cdd4b319569cc48bb9507a64d6dd2deb9a07e6
chris <chrissakwa@gmail.com>
2022-09-12 02:10:45 -0700

    REST API Development Best Practces,  Automapper and Repositories

---
+++ HotelListing.API/Contracts/ICountriesRepository.cs
@@ -0,0 +1,13 @@
+﻿using HotelListing.API.Data;
+
+namespace HotelListing.API.Contracts
+{
+    public interface ICountriesRepository : IGenericRepository<Country>
+    {
+        Task<Country> GetDetails(int id);
+    }
+
+
+
+
+}
